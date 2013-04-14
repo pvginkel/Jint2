15d4f43017dd7f8631236535c7eade5b7d90d6d3
Pieter van Ginkel <pvginkel@gmail.com>
2013-04-14 11:13:01 +0200

    Added overloads to correctly report lexer/parser errors.

---
+++ Jint/Parser/ES3Additions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Antlr.Runtime;
+
+partial class ES3Lexer
+{
+    public override void ReportError(RecognitionException e)
+    {
+        throw e;
+    }
+
+    protected override object RecoverFromMismatchedToken(IIntStream input, int ttype, BitSet follow)
+    {
+        throw new MismatchedTokenException(ttype, input);
+    }
+
+    public override object RecoverFromMismatchedSet(IIntStream input, RecognitionException e, BitSet follow)
+    {
+        throw e;
+    }
+}
+
+partial class ES3Parser
+{
+    public override void ReportError(RecognitionException e)
+    {
+        throw e;
+    }
+
+    protected override object RecoverFromMismatchedToken(IIntStream input, int ttype, BitSet follow)
+    {
+        throw new MismatchedTokenException(ttype, input);
+    }
+
+    public override object RecoverFromMismatchedSet(IIntStream input, RecognitionException e, BitSet follow)
+    {
+        throw e;
+    }
+}
