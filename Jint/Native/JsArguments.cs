ce625f34f0240166c14d9b0cc7ea6355bc13c95d
Pieter van Ginkel <pvginkel@gmail.com>
2013-11-23 09:02:32 +0100

    Length property of arguments object is a normal, configurable, property.

--- Jint/Native/JsArguments.cs
+++ Jint/Native/JsArguments.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 using Jint.Delegates;
 
@@ -9,52 +10,35 @@ namespace Jint.Native
     public class JsArguments : JsObject
     {
         public const string CalleeName = "callee";
-
-        private readonly ValueDescriptor _calleeDescriptor;
-
-        protected JsFunction Callee
-        {
-            get { return this[CalleeName] as JsFunction; }
-            set { this[CalleeName] = value; }
-        }
+        private const string LengthPropertyName = "length";
 
         public JsArguments(IGlobal global, JsFunction callee, JsInstance[] arguments)
             : base(global.ObjectClass.New())
         {
-            _global = global;
+            int length;
 
             // Add the named parameters
             if (arguments != null)
             {
                 for (int i = 0; i < arguments.Length; i++)
                 {
-                    DefineOwnProperty(
-                        new ValueDescriptor(this, i.ToString(), arguments[i]) { Enumerable = false }
-                    );
+                    DefineOwnProperty(new ValueDescriptor(this, i.ToString(CultureInfo.InvariantCulture), arguments[i]) { Enumerable = false });
                 }
 
-                _length = arguments.Length;
+                length = arguments.Length;
             }
             else
             {
-                _length = 0;
+                length = 0;
             }
 
-            _calleeDescriptor = new ValueDescriptor(this, CalleeName, callee) { Enumerable = false };
-            DefineOwnProperty(_calleeDescriptor);
-
-            DefineOwnProperty(new PropertyDescriptor<JsArguments>(global, this, "length", GetLength) { Enumerable = false });
+            DefineOwnProperty(new ValueDescriptor(this, CalleeName, callee) { Enumerable = false });
+            DefineOwnProperty(new ValueDescriptor(this, LengthPropertyName, global.NumberClass.New(length)) { Enumerable = false, Configurable = true });
         }
-
-        private int _length;
-        private readonly IGlobal _global;
 
         public override bool IsClr
         {
-            get
-            {
-                return false;
-            }
+            get { return false; }
         }
 
         public override bool ToBoolean()
@@ -64,32 +48,12 @@ namespace Jint.Native
 
         public override double ToNumber()
         {
-            return Length;
-        }
-
-        /// <summary>
-        /// The number of the actually passed arguments
-        /// </summary>
-        public override int Length
-        {
-            get
-            {
-                return _length;
-            }
-            set
-            {
-                _length = value;
-            }
+            return this[LengthPropertyName].ToNumber();
         }
 
         public override string Class
         {
             get { return ClassArguments; }
-        }
-
-        public JsInstance GetLength(JsArguments target)
-        {
-            return _global.NumberClass.New(target._length);
         }
     }
 }
